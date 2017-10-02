686334a2f7172e8c1e427289138cfd5980786534
AndyLeslie <1286492310@qq.com>
2017-10-03 01:22:28 +0800

    first commit

---
+++ hdpjob/src/main/java/com/abunba/log/job/LogJob.java
@@ -0,0 +1,21 @@
+package com.abunba.log.job;
+
+import org.apache.log4j.Logger;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+@Lazy(false)
+public class LogJob   {
+	
+	private  static Logger logger = Logger.getLogger(LogJob.class);
+
+//    @Scheduled(cron = "0 30 0 * * ?")
+    @Scheduled(cron = "0,30,59 * * * * ?")
+    public void synRecipeCount() {
+    	System.out.println("ppppp");
+    }
+
+
+}
